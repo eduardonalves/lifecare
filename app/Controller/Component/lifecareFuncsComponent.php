d0485192475a4502a8d1545be2805a3690245f4a
Eduardo Alves <eduardonalves@gmail.com>
2014-04-03 09:56:52 -0300

    Converter moeda para banco

--- app/Controller/Component/lifecareFuncsComponent.php
+++ app/Controller/Component/lifecareFuncsComponent.php
@@ -14,7 +14,30 @@ class lifecareFuncsComponent extends Component {
 		return $valorMoeda;
 	}
     
-	
+	public function converterMoedaToBD(&$valorMoeda){
+		$valorMoedaAux = explode('.' , $valorMoeda);
+		if(isset ($valorMoedaAux[1])){
+			$i = 0;
+			$convertido='';
+			foreach($valorMoedaAux as $valor){
+				$convertido =$convertido.$valor[$i]; 
+			}
+		}else{
+			$convertido = $valorMoedaAux[0];
+		}
+		
+		$decimal = explode(',' , $convertido);
+		
+		
+		if(isset($decimal[1])){
+			$valorFinal=$decimal[0].'.'.$decimal[1];
+		}else{
+			$valorFinal=$decimal[0];
+		}
+		$valorMoeda=$valorFinal;
+		
+		return $valorFinal;
+	}
 
 
 	
