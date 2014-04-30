f9a7104a7bed55712775f3a6a8fa55c3353f8afc
ChiCopyleft <info.francisco.depaula@gmail.com>
2014-04-30 16:10:20 -0300

    Ajuste edit centro custo

--- app/webroot/js/funcoes_centrocusto.js
+++ app/webroot/js/funcoes_centrocusto.js
@@ -35,7 +35,15 @@ $(document).ready(function() {
 		mes = mes.toString();
 		$('#limite'+numero).html('<input type="hidden" name="data[Orcamentocentro]['+numero+'][periodo_final]" value="'+$('#CentrocustoGetY').val()+'-'+mes+'-'+'30" id="periodo_final" >');
 		$('#limite'+numero).append('<span id="validaAddLimite" class="Msg-tooltipDireita" style="display:none">Preencha o Limite</span>');
-		$('#limite'+numero).append('<input type="text" name="data[Orcamentocentro]['+numero+'][limite]" value="0.00" id="Orcamentocentro'+numero+'Id" class="tamanho-medio">');
+		$('#limite'+numero).append('<input type="text" name="data[Orcamentocentro]['+numero+'][limite]" value="0.00" id="Orcamentocentro'+numero+'Id" class="tamanho-medio Nao-Letras dinheiro_duasCasas">');
+
+		$(".dinheiro_duasCasas").priceFormat({
+	    prefix: '',
+	    centsSeparator: ',',
+	    thousandsSeparator: '.',
+	    limit: 15
+	});
+
 
 	});
 	
