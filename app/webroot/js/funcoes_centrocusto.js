cfd56c822e5221cb4c1e49918c5457e501d37e3c
ChiCopyleft <info.francisco.depaula@gmail.com>
2014-04-30 14:25:19 -0300

    Edit centro custo

--- app/webroot/js/funcoes_centrocusto.js
+++ app/webroot/js/funcoes_centrocusto.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
 		var mes = parseInt(numero) + 1;
 		mes = mes.toString();
 		$('#limite'+numero).html('<input type="hidden" name="data[Orcamentocentro]['+numero+'][periodo_final]" value="'+$('#CentrocustoGetY').val()+'-'+mes+'-'+'30" id="periodo_final" >');
-		$('#limite'+numero).append('<input type="text" name="data[Orcamentocentro]['+numero+'][limite]" value="0.00" id="Orcamentocentro'+numero+'Id">');
+		$('#limite'+numero).append('<input type="text" name="data[Orcamentocentro]['+numero+'][limite]" value="0.00" id="Orcamentocentro'+numero+'Id" class="tamanho-medio">');
 	});
 	
 	$('.btneditar').click(function(e){
