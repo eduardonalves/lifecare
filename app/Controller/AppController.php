66c42b0fc05441876dd21f843af550d771009c9e
HenriRique <henriquesb92@gmail.com>
2014-05-27 13:24:28 -0300

    inicio módulo compras

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -87,11 +87,12 @@ class AppController extends Controller {
 			);
 			
 			
-			/*$this->Auth->authorize = array(
+		$this->Auth->authorize = array(
 				AuthComponent::ALL => array('actionPath' => 'controllers'),
 				//'Actions'
 			);
-		*/
+		
+		
 			
 			
 			$this->Auth->loginAction = array(
