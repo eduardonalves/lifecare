a92cdb40d28c58c57145432d47075ea31e4aa393
HenriRique <henriquesb92@gmail.com>
2014-05-30 14:04:57 -0300

    pedidos

--- app/Controller/PedidosController.php
+++ app/Controller/PedidosController.php
@@ -15,8 +15,55 @@ class PedidosController extends ComoperacaosController {
  *
  * @var array
  */
-	public $components = array('Paginator');
-
+	public $components = array('Paginator','lifecareDataFuncs');
+	
+	
+	
+	public $tiposUnidades;
+	
+	private function loadUnidade(){
+		 $this->tiposUnidades = array(
+				'UN'=>'Unidade',
+				'PC'=>'Peça',
+				'CX'=>'Caixa',
+				'CJ'=>'Conjunto',
+				'KG'=>'Kilo',
+				'G'=>'Grama',
+				'M'=>'Metro',
+				'BOL' => 'Bolsa',
+				'BIS' => 'Bisnaga',
+				'SCH' => 'Sachê',
+				'PCT' => 'Pacote',
+				'ENV' => 'Envelope',
+				'PAR' => 'Pares',
+				'M2'=>'M. Quadrado', 
+				'M3' =>'M. Cúbico',
+				'L' =>'Litro',
+				'DZ' => 'Dúzia',
+				'SAC' => 'Saco',
+				'H' => 'Hora',
+				'CM' => 'Centímetro',
+				'T' => 'Tonelada',
+				'CJ' => 'Conjunto',
+				'KIT' => 'Kit',
+				'MIL' => 'Milheiro',
+				'JG' => 'Jogo',
+				'MM' => 'Milímetro',
+				'GL' => 'Galão',
+				'RSM' => 'Resma',
+				'FD' => 'Fardo',
+				'BL' =>'Bloco',
+				'AP' => 'Ampola',
+				'FR' => 'Frasco',
+				'CP' => 'Comprimido',
+				'TB' => 'Tubo',
+				'F/A' => 'Frasco/Ampola'
+			);
+			asort($this->tiposUnidades);
+			$this->tiposUnidades = array(''=>'') + $this->tiposUnidades;
+		$this->set('tiposUnidades', $this->tiposUnidades);
+	}
+	
 /**
  * index method
  *
@@ -35,6 +82,7 @@ class PedidosController extends ComoperacaosController {
  * @return void
  */
 	public function view($id = null) {
+		$this->layout = 'compras';
 		if (!$this->Pedido->exists($id)) {
 			throw new NotFoundException(__('Invalid pedido'));
 		}
@@ -47,18 +95,38 @@ class PedidosController extends ComoperacaosController {
  *
  * @return void
  */
-	public function add() {
+	public function add(){
+		$this->layout = 'compras';
+		$userid = $this->Session->read('Auth.User.id');
+		$this->loadUnidade();
+		$this->lifecareDataFuncs->formatDateToBD($this->request->data['Comoperacao']['data_inici']);
+		$this->lifecareDataFuncs->formatDateToBD($this->request->data['Comoperacao']['data_fim']);
+		
+		//debug($this->request->data);
 		if ($this->request->is('post')) {
 			$this->Pedido->create();
-			if ($this->Pedido->save($this->request->data)) {
+			if ($this->Pedido->saveAll($this->request->data)) {
 				$this->Session->setFlash(__('The pedido has been saved.'));
 				return $this->redirect(array('action' => 'index'));
-			} else {
+			}else{
 				$this->Session->setFlash(__('The pedido could not be saved. Please, try again.'));
 			}
 		}
+		
+		$this->loadModel('Produto');
+		$produtos = $this->Produto->find('all', array('recursive' => -1,'order' => 'Produto.nome ASC'));
+
+		$this->loadModel('Parceirodenegocio');
+		$parceirodenegocios = $this->Parceirodenegocio->find('all', array('recursive' => -1,'order' => 'Parceirodenegocio.nome ASC','conditions' => array('Parceirodenegocio.tipo' => 'FORNECEDOR')));
+		
+		$categorias = $this->Produto->Categoria->find('list', array('order'=>'Categoria.nome ASC'));
+		$allCategorias = $categorias;
+		
+		$categorias = array('add-categoria'=>'Cadastrar') + $categorias;
+		
+		
 		$users = $this->Pedido->User->find('list');
-		$this->set(compact('users'));
+		$this->set(compact('users','produtos','parceirodenegocios','userid','allCategorias','categorias'));
 	}
 
 /**
@@ -69,6 +137,10 @@ class PedidosController extends ComoperacaosController {
  * @return void
  */
 	public function edit($id = null) {
+		$this->layout = 'compras';
+		$userid = $this->Session->read('Auth.User.id');
+		
+		
 		if (!$this->Pedido->exists($id)) {
 			throw new NotFoundException(__('Invalid pedido'));
 		}
@@ -83,7 +155,8 @@ class PedidosController extends ComoperacaosController {
 			$options = array('conditions' => array('Pedido.' . $this->Pedido->primaryKey => $id));
 			$this->request->data = $this->Pedido->find('first', $options);
 		}
-		$users = $this->Pedido->User->find('list');
+				
+		$users = $this->Comoperacao->User->find('list');
 		$this->set(compact('users'));
 	}
 
@@ -94,7 +167,7 @@ class PedidosController extends ComoperacaosController {
  * @param string $id
  * @return void
  */
-	public function delete($id = null) {
+public function delete($id = null) {
 		$this->Pedido->id = $id;
 		if (!$this->Pedido->exists()) {
 			throw new NotFoundException(__('Invalid pedido'));
