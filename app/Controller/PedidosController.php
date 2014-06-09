afb3cadfe448dace4e2f6c683b675becdad0d036
Eduardo Alves <eduardonalves@gmail.com>
2014-06-09 13:56:17 -0300

    Alteracoes Pedidos

--- app/Controller/PedidosController.php
+++ app/Controller/PedidosController.php
@@ -140,9 +140,14 @@ class PedidosController extends ComoperacaosController {
 					$produto = $this->Produto->find('first', array('conditions' => array('Produto.id' => $ultimoPedido['Comitensdaoperacao'][$id]['produto_id'])));
 					$ultimoPedido['Comitensdaoperacao'][$id]['produtoNome'] = $produto['Produto']['nome']; 	
 					//Relacionamos fornecedores a produtos
-					$upProdFornec = array('parceirodenegocio_id' => $this->request->data['Parceirodenegocio'][0]['parceirodenegocio_id'], 'produto_id' =>  $ultimoPedido['Comitensdaoperacao'][$id]['produto_id']);
-					$this->ProdutosParceirodenegocio->create();
-					$this->ProdutosParceirodenegocio->save($upProdFornec);
+					
+					$inter= $this->ProdutosParceirodenegocio->find('first', array('conditions' => array('ProdutosParceirodenegocio.parceirodenegocio_id'=>  $this->request->data['Parceirodenegocio'][0]['parceirodenegocio_id'], 'AND' => array('produto_id' =>  $ultimoPedido['Comitensdaoperacao'][$id]['produto_id']))));
+					if(!empty($inter)){
+						$upProdFornec = array('parceirodenegocio_id' => $this->request->data['Parceirodenegocio'][0]['parceirodenegocio_id'], 'produto_id' =>  $ultimoPedido['Comitensdaoperacao'][$id]['produto_id']);
+						$this->ProdutosParceirodenegocio->create();
+						$this->ProdutosParceirodenegocio->save($upProdFornec);
+					}
+					
 					
 				}
 				
