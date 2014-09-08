811af9d6e6c3b17c88ce37ae1b8cf15095072b99
Eduardo Alves <eduardonalves@gmail.com>
2014-09-08 11:34:49 -0300

    Alt prod

--- app/Controller/CotacaosController.php
+++ app/Controller/CotacaosController.php
@@ -129,7 +129,7 @@ class CotacaosController extends ComoperacaosController {
 			$email->emailFormat('html');
 			
 			//essa linha só serve para o servidor da alemanha
-			//$email->transport('Mail');
+			$email->transport('Mail');
 
             if($email->send($mensagem)){
 				return TRUE;
