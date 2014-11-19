3642e07fbdda52fea8fe227bb19f9595623d751a
Henrique <henriquesb92@gmail.com>
2014-11-19 19:51:45 -0200

    atualização repositório

--- app/webroot/js/separacaoLotes.js
+++ app/webroot/js/separacaoLotes.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
 	
-	
 	$('.orglotes').click(function(){
 		$('.loaderAjaxCarregarLoteDIV').show();
 		var produto_id = $(this).attr('data-produtoId');
-			
+		var nid = $(this).attr('id');
+		nid = nid.substring(7);
+		
 		$("#carregaSelect").load(urlInicio+'lotes/carregalotevalidade?numero='+produto_id+'', function(){
 			$('.loaderAjaxCarregarLoteDIV').hide();
 			$('#myModal_add-troca_lote').modal('show');
-		});
-		
-		
+			$('#identificacao').val(nid);
+		});		
 	});
+		
+	//lote_venda
 		
 
 });
