7f9bcd167733de0e6847414f2f5077bdd3bf019e
Glauber Sátiro <glaubersatiro@gmail.com>
2015-03-02 12:01:21 -0300

    Transportadores Filtro

--- app/Controller/TransportadoresController.php
+++ app/Controller/TransportadoresController.php
@@ -41,8 +41,32 @@ class TransportadoresController extends ParceirodenegociosController {
  * @return void
  */
 	public function index() {
-		$this->Transportadore->recursive = 0;
-		$this->set('transportadores', $this->Paginator->paginate());
+
+		$this->Filter->addFilters(
+	        array(
+	            'transportadorNome' => array(
+	                'Transportadore.nome' => array(
+	                    'operator' => 'LIKE'
+
+	                )
+	            )
+	            ));
+
+			$conditions = $this->Filter->getConditions();
+
+			//$this->Transportadore->find('all',array('conditions'=>$conditions, 'recursive' => 1));
+
+			$this->Paginator->settings = array(
+				'Transportadore' => array(
+					'limit' => $this->request['url']['limit'],
+					'conditions' => $conditions,
+					'order' => 'Transportadore.nome asc'
+				)
+			);			
+
+			$transportadores = $this->Paginator->paginate('Transportadore');
+			$this->set(compact('transportadores'));
+
 	}
 
 /**
